e4fa32f416569fb5b1f9f99a39ccaa620e0b0a88
kanishk1010 <kanishk.dugar@outlook.com>
2017-10-10 22:28:13 -0400

    implemented lab1

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -7,6 +7,9 @@ import "fmt"
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
+	job int
+	available bool
+	jobType JobType
 }
 
 
@@ -30,5 +33,44 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	processJob(mr, Map, mr.nMap, mr.nReduce)
+	processJob(mr, Reduce, mr.nReduce, mr.nMap)
 	return mr.KillWorkers()
+}
+
+func processJob(mr *MapReduce,jobType JobType, jobs int, otherJobs int, ){
+
+	for i := 0; i < jobs; i++ {
+		go func(jobNum int) {
+			done := false
+			var worker string
+			for !done {
+				worker = <-mr.registerChannel
+				done = call(worker, "Worker.DoJob",
+					DoJobArgs{mr.file, jobType, jobNum, otherJobs}, new(DoJobReply))
+			}
+			mr.activeWorkers <- worker // channel of active workers
+
+			// worker registration
+			workerInfo := new(WorkerInfo)
+			workerInfo.job = jobNum
+			workerInfo.available = false
+			workerInfo.jobType = jobType
+			mr.Workers[worker] = workerInfo
+
+			mr.registerChannel <- worker // making worker available for reuse
+		}(i)
+	}
+
+	//All maps have to finish before reduce can begin
+	for j := 0; j < jobs; j++ {
+
+		worker:= <-mr.activeWorkers
+		val, ok := mr.Workers[worker]
+		if ok{
+			val.available = true
+			val.jobType = ""
+			val.job = -1
+		}
+	}
 }
